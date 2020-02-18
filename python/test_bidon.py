832afe4f7947b39bf2f3a6e970b550cbaa846b9a
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2020-02-18 20:56:59 +0100

    Update test_bidon.py

--- python/test_bidon.py
+++ python/test_bidon.py
@@ -15,20 +15,31 @@ def open_live_tchat(number_of_message,chat_name):
         number_of_message: Number of message you want to save.
         chat_name: Name of the .json file where you want to save the messages
     """
+
     chat = {
-        'Senders':[],
-        'Messages':[]
+        'Message':[],
+        'Compteur':0
     }
 
     def _add_to_chat(message):
         """Function that print a message, and save it inside a dict.
         """
 
-        print('{} -> {}'.format(message.sender,message.text))
-        chat['Senders'].append(message.sender)
-        chat['Messages'].append(message.text)
+        print('({}) - {} -> {}'.format(chat['Compteur'],message.sender,message.text))
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-        if len(chat['Messages']) > number_of_message:
+        msg_dict = {
+            'Message_id':chat['Compteur'],
+            'Time':dt_string,
+            'Sender':message.sender,
+            'Text':message.text
+        }
+        chat['Compteur'] += 1
+        
+        chat['Message'].append(msg_dict)
+
+        if chat['Compteur'] > number_of_message:
             with open(name_chat, 'w') as fp:
                 print('Saving chat...')
                 json.dump(chat, fp)
@@ -39,7 +50,7 @@ def open_live_tchat(number_of_message,chat_name):
             lambda message: _add_to_chat(message))
 
 
-open_live_tchat(number_of_message=100,chat_name=name_chat)
+open_live_tchat(number_of_message=10000,chat_name=name_chat)
 
 
 
